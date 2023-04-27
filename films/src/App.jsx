7ff2c47a671cd32fe0e24a7b962979d188cbc925
Rayane Naboulsi <rayanenaboulsi02@gmail.com>
2023-04-28 00:24:51 +0300

    init-Complete

---
+++ films/src/App.jsx
@@ -0,0 +1,69 @@
+import {useEffect, useState} from "react";
+
+import './App.css'
+import searchIcon from './search.svg';
+import MovieCard from "./MovieCard.jsx";
+
+function App() {
+    const API_URL = "https://www.omdbapi.com?apikey=aa90bb7a";
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState([]);
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        //https://www.omdbapi.com?apikey=58324415&s=telugu
+        const data = await response.json();
+        setMovies(data.Search);
+    };
+
+    useEffect(() => {
+        // searchMovies();
+    }, [searchTerm]);
+
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+            searchMovies(searchTerm);
+        }
+    };
+
+    return (
+        <div className="app">
+            <div className="fade-in">
+              <h1>FilmWorld</h1>
+            </div>
+            <div className="search">
+                <input
+                    placeholder="Search for Movies"
+                    alt="movieSearch"
+                    value={searchTerm}
+                    onChange={(e) => {
+                        setSearchTerm(e.target.value);
+                    }}
+                    onKeyDown={handleKeyPress}
+                />
+                <img
+                    src={searchIcon}
+                    alt="searchIcon"
+                    onClick={() => {
+                        searchMovies(searchTerm);
+                    }}/>
+            </div>
+            {movies?.length > 0 ?
+                <div className="container">
+                    {movies.map((movie) => (
+                        <div>
+                            <MovieCard Movie={movie}/>
+                        </div>
+                    ))}
+
+                </div> : <div className="empty">
+                    <h3 style={{color: 'white'}}>No Movies !! :( </h3>
+                </div>
+            }
+
+
+        </div>
+    )
+}
+
+export default App;
